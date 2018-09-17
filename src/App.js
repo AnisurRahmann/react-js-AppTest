27d9dd5caacac99c1b20dd9a0c496a74e0c2a609
Shakil <pshakilwizard@gmail.com>
2018-09-18 01:23:12 +0600

    adding header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+import Header from './components/Header/Header'
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>I am running</h1>
+        <Header />
       </div>
     );
   }
